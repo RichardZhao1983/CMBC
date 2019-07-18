09c71355d8979fdcc18de6dcd2f15b6b5f5b0e68
RichardZhao1983 <weidi1983@163.com>
2019-07-18 20:35:14 +0800

    Update Gruntfile_ABAP.js

--- Gruntfile_ABAP.js
+++ Gruntfile_ABAP.js
@@ -17,8 +17,8 @@ module.exports = function(grunt) {
     var jobURL = process.env.JOB_URL;
     var nexusSnapshotRepoURL = process.env.NEXUS_SNAPSHOT_REPO;
     var gitCommit = process.env.GIT_COMMIT;
-    var username = process.env.Credentials.bitbucket.username;
-    var password = process.env.Credentials.bitbucket.Password;
+    var username = process.env.Credentials.Username;
+    var password = process.env.Credentials.Password;
     // Global Variables
     var zipDir = "dist";
     var zipFileSuffix = ".zip";
